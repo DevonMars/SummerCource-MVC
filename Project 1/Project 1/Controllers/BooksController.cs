1ecafc898988b7fc299fd51d026153c8c50ea3ff
Devon MarshDdam <fdfsafsffsf>
2018-07-07 03:40:45 +0200

    Create method and view added

---
+++ Project 1/Project 1/Controllers/BooksController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _repository;
+
+        public BooksController(ApplicationDbContext repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.Books.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create (Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                _repository.Add(book);
+                await _repository.SaveChangesAsync();
+                return RedirectToAction("Index");
+                    
+            }
+
+            return View(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repository.Dispose();
+            }
+        }
+    }
+}
