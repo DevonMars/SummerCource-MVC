ed70ff2e7ef5ce87468dd370949ceb3a6ab1ba15
Devon MarshDdam <fdfsafsffsf>
2018-07-08 03:10:34 +0200

    Details & edit & Delete action added to booklist

--- Project 1/Project 1/Controllers/BooksController.cs
+++ Project 1/Project 1/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project1.Models;
 
 namespace Project1.Controllers
@@ -41,6 +42,83 @@ namespace Project1.Controllers
 
             return View(book);
         }
+
+
+        public async Task<IActionResult> Details (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _repository.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _repository.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            if( id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _repository.Update(book);
+                await _repository.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _repository.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveBook(int id)
+        {
+            var book = await _repository.Books.SingleOrDefaultAsync(m => m.Id == id);
+            _repository.Books.Remove(book);
+            await _repository.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
