0d12d36f48e86bd77e2895374afc435794fe6549
Devon MarshDdam <fdfsafsffsf>
2018-07-09 03:17:48 +0200

    Project 2

---
+++ Project2CatagoryBootstrap/Project2CatagoryBootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project2CatagoryBootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
