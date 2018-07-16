454280b698fb9ec8b61bede7de154a1e083bb6a6
Devon MarshDdam <fdfsafsffsf>
2018-07-16 06:23:41 +0200

    adding stuff

--- Project2CatagoryBootstrap/Project2CatagoryBootstrap/Data/ApplicationDbContext.cs
+++ Project2CatagoryBootstrap/Project2CatagoryBootstrap/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace Project2CatagoryBootstrap.Data
         {
         }
 
+        DbSet<Category> Category { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
--- Project2CatagoryBootstrap/Project2CatagoryBootstrap/Data/Migrations/20180710180438_addCategoryToDatabase.Designer.cs
+++ Project2CatagoryBootstrap/Project2CatagoryBootstrap/Data/Migrations/20180710180438_addCategoryToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Project2CatagoryBootstrap.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180710180438_addCategoryToDatabase")]
+    partial class addCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
